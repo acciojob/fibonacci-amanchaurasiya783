b1dcc888c4b724a9a6cbb56e80ff8331614292c6
amanchaurasiya783 <127719170+amanchaurasiya783@users.noreply.github.com>
2023-07-27 12:48:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function fibonacci(num) {
 // your code here
-	var i = 0;
+	if(num==0){
+		return 0;
+	}else if(num==1){
+		return 1;
+	}
+	var i = 2;
 	let a = 0;
 	let b = 1;
 	while (i<=num) {
@@ -11,5 +16,3 @@ function fibonacci(num) {
 	}
 	return b;
 }
-
-module.exports = fibonacci;
